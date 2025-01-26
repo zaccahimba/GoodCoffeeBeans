503d0bed6fa32bd5e16f0825288355b4696df929
IvelisBecker <ivelisbecker@gmail.com>
2025-01-26 14:48:17 -0600

    Now Title and Image from Flavor of the Day worked correctly

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 const carousel = document.querySelector('#randomFlavorOfTheDay');
 const images = document.querySelector('#coffee'); 
+const coffeeName = document.querySelector('#coffeeName');
 
 const next = carousel.querySelector('.next');
 const prev = carousel.querySelector('.prev');
 let index = 0;
-let currentImage;
+// let currentImage;
 
-const image = [
+const coffeeNames = [
+    'Butterscotch Latte',
+    'Peppermint Mocha',
+    'Spicy Pumpkin Latte',
+]
+
+
+const coffeeImage = [
     './images/butterscotch-latte.jpg',
     './images/peppermint_mocha_latte.jpg',
     './images/spicy-pumpkin-latte.jpg'
@@ -17,14 +25,14 @@ const image = [
 function navigate(direction) {
     index = index + direction;
     if (index < 0) {
-        index = images.length - 1;
-    } else if (index > images.length - 1) {
+        index = coffeeNames.length - 1;
+    } else if (index > coffeeName.length - 1) {
         index = 0;
     }
-    currentImage = image[index];
-    images.setAttribute('src', currentImage); 
-    //TODO: add an .style to fix size of images
-}
+
+    images.setAttribute('src', coffeeImage[index]);
+    coffeeName.textContent = coffeeNames[index];
+};
 
 next.addEventListener('click', () => navigate(1)); 
 prev.addEventListener('click', () => navigate(-1));
